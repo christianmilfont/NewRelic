62aae36feb7d0661599372b61ef722c81a46def7
Christian Neres Severino Martins Milfont <67718947+christianmilfont@users.noreply.github.com>
2025-05-10 19:04:56 -0300

    Update otel-bootstrap.ts

--- order-mngt-api-1.0.0-newrelic/otel-bootstrap.ts
+++ order-mngt-api-1.0.0-newrelic/otel-bootstrap.ts
@@ -5,7 +5,7 @@ import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
 // Configuração do logger do OpenTelemetry
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
 
-const newRelicApiKey = '63e29ebb818309dd859e41d256ee2113FFFFNRAL" ';
+const newRelicApiKey = 'USEIMINHAAPIKEY" ';
 
 // Exportador OTLP para enviar os dados para o New Relic
 const traceExporter = new OTLPTraceExporter({
