be79e2e2765494a318dcd9a9c85ec8e81e7ec10c
Christian Neres Severino Martins Milfont <67718947+christianmilfont@users.noreply.github.com>
2025-05-11 23:01:47 -0300

    Update newrelic.cjs

--- order-mngt-api-1.0.0-newrelic/newrelic.cjs
+++ order-mngt-api-1.0.0-newrelic/newrelic.cjs
@@ -1,6 +1,6 @@
 exports.config = {
   app_name: ['order-mngt-api'],  // Nome da sua aplicação
-  license_key: '63e29ebb818309dd859e41d256ee2113FFFFNRAL', // Coloque sua chave de licença aqui
+  license_key: '', // Coloque sua chave de licença aqui
   logging: {
     level: 'info',
   },
